7049190fd9616900bcf97c2e0c514ca0d9e16041
mynameisvinn <vin.tang@gmail.com>
2020-05-14 10:24:45 -0400

    work on remote execution from server

--- Kick/connection/barfoo.py
+++ Kick/connection/barfoo.py
@@ -1,4 +1,4 @@
-
+# test
 
 import numpy as np
 
@@ -8,5 +8,4 @@ b = np.random.rand(4, 4)
 def matmul(a, b):
     return a.dot(b)
 
-if __name__ == "__main__":
-    print(matmul(a, b))
+res = matmul(a, b)
