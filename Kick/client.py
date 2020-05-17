5c7b48155e463a89fe2f4d4e37dd9a13da8072bf
mynameisvinn <vin.tang@gmail.com>
2020-05-17 10:44:18 -0400

    send arbitrary python objects - not just np arrays - through socket

--- Kick/client.py
+++ Kick/client.py
@@ -1,12 +1,23 @@
 import socket
 import numpy as np
+import jsonpickle
+
+def from_bytes(b):
+    """convert bytes back to python object.
+
+    convert bytes -> json -> python object.
+    """
+    j = b.decode()  # from bytes to json
+    o = jsonpickle.decode(j)  # from json to python object
+    return o
+
 
 def up(port, fname):
     """send fname to server and retrieve corresponding results.
     
     # https://stackoverflow.com/questions/9382045/send-a-file-through-sockets-in-python
     """
-    # create socket and connect
+    # create socket and connect with server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('localhost', port)) 
 
@@ -15,13 +26,14 @@ def up(port, fname):
         l = f.read(1024)
     s.send(l) 
         
-    # receive results from server and unpack bytes to numpy
+    # receive results from server and unpack bytes
     res = s.recv(1024)
-    res_unpacked = np.frombuffer(res)  # https://markhneedham.com/blog/2018/04/07/python-serialize-deserialize-numpy-2d-arrays/
+    o = from_bytes(res)  # https://markhneedham.com/blog/2018/04/07/python-serialize-deserialize-numpy-2d-arrays/
+    print(o)
     
     # close the connection 
     if res:
         s.close()
-        return res_unpacked
+        return o
     else:
         raise ValueError
--- connection/server.py
+++ connection/server.py
@@ -1,6 +1,8 @@
 """
 spin up server for remote execution. 
 
+server application should be run on ec2 instance with $ python server.py
+
 https://www.geeksforgeeks.org/socket-programming-python/
 """
 
@@ -8,27 +10,26 @@ import socket
 import numpy as np
 import sys
 import subprocess
+import jsonpickle
 
 def execute(fname):
-    """
-    # https://www.semicolonworld.com/question/59697/return-value-from-one-python-script-to-another
-    s2_out = subprocess.check_output([sys.executable, fname])
-    return s2_out
+    """evaluate python source and return res to client.
+
+    evaluate python source and place its runtime environment into namespace, a dict. then, extract res
+    from environment and return it to client.
     """
     
     # https://github.com/mynameisvinn/piegrad/blob/master/PieGrad.py
     # https://stackoverflow.com/questions/16877323/getting-return-information-from-another-python-script
     # https://stackoverflow.com/questions/6357361/alternative-to-execfile-in-python-3
-    namespace={}  # environment
+    namespace={}
     with open(fname, "rb") as source_file:
         code = compile(source_file.read(), fname, "exec")
     exec(code, namespace)  # put results in namespace env 
     
     # send numpy results as bytes https://markhneedham.com/blog/2018/04/07/python-serialize-deserialize-numpy-2d-arrays/
-    # could picklet
     res = namespace['res']
-    res_bytes = res.tobytes()
-    return res_bytes
+    return res
     
 
 def receive_file(c, out_fname):
@@ -40,35 +41,51 @@ def receive_file(c, out_fname):
 
     l = c.recv(1024)
     print("reading", l)
-    f = open(out_fname,'wb')
-    f.write(l)
-    f.close()
+    with open(out_fname,'wb') as f:
+        f.write(l)
     return True
 
 
+def to_bytes(o):
+    """convert arbitrary python object to bytes.
+    """
+    j = jsonpickle.encode(o)  # python object to json, which is a str  # https://www.journaldev.com/23500/python-string-to-bytes-to-string    
+    b = bytes(j, encoding='utf-8')  # json to bytes# https://www.journaldev.com/23500/python-string-to-bytes-to-string
+    return b
+
+
+def from_bytes(b):
+    """convert bytes back to python object.
+    """
+    j = b.decode()  # from bytes to json
+    o = jsonpickle.decode(j)  # from json to python object
+    return o
 
 if __name__ == '__main__':
-    print("server up")
+    print(">> server up")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    endpoint = ('', 4001)  # https://stackoverflow.com/questions/8033552/python-socket-bind-to-any-ip
+    endpoint = ('', 4002)  # https://stackoverflow.com/questions/8033552/python-socket-bind-to-any-ip
     s.bind(endpoint)
     s.listen(5)
     while True:
 
-        # connect with whoever pinged server
+        # step 1: connect with incoming clinet
         c, addr = s.accept()      
         print('connected to', addr )
 
-        # receive source file from client
+        # step 2: receive source file from client and save it
         temp_fname = "barfoo.py"
         _ = receive_file(c, temp_fname)
 
-        # server evaluates source and returns results
-        res = execute(temp_fname)  # res is numpy answer in bytes
-        print(np.frombuffer(res))
+        # step3 3: evaluates source and return result
+        res = execute(temp_fname)
 
-        # return result back to client
-        c.send(res)  # res is sent as bytes, so it will have to be converted back to numpy array
+        # step 4: convert results to bytes so it can be sent over socket
+        b = to_bytes(res)
+        print(from_bytes(b))
+
+        # step 5: send bytes to client
+        c.send(b)
 
         # close connection 
         c.close() 
